040901b52dd991daa0674da2fbb1b531fe54e477
Dmitriy Korolov <adrorecords@gmail.com>
2022-03-19 09:32:16 +0200

    done configs

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const swaggerUI = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerConfig = require("./configs/swagger-config.json");
+const cookieParser = require("cookie-parser");
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,10 +19,14 @@ app.use(cors());
 
 // app.use("/policeUser", policeUserRouter);
 
-app.use((req, res, next) => {
-  res.status(500).send("Something broke!");
-});
+const start = async () => {
+  try {
+    app.listen(PORT, () =>
+      console.log(`Server has been started on port ${PORT}`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-app.listen(PORT, () => {
-  console.log(`Server has been started on port ${PORT}`);
-});
+start();
